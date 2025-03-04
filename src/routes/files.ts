64a14538a4751355f03ad4f6390de727330c6043
Phylosius <hei.phylosius@gmail.com>
2025-03-04 14:55:47 +0300

    feat: created GET /files/download/:fileID

--- src/routes/files.ts
+++ src/routes/files.ts
@@ -10,4 +10,12 @@ app.post('/upload', upload.single('file'), (req: Request, res: Response) => {
     res.json(req.file)
 })
 
+app.get('/download/:fileID', (req: Request, res: Response) => {
+    const fileID = req.params.fileID;
+    
+    res.download(`uploads/${fileID}`, `${fileID}`, (err)=>{
+        res.status(500).send(`error: ${err}`);
+    })
+})
+
 export default app;
