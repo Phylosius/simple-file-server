b92b79e5b1107782f33a9498045157dbe6fe30ef
Phylosius <hei.phylosius@gmail.com>
2025-03-04 16:31:46 +0300

    refactor: changed files db and sql on services into typescript files

---
+++ src/services/db.ts
@@ -0,0 +1,36 @@
+import sql from './sql';
+
+interface FileInfo {
+    id: string,
+    name: string,
+    mimetype: string,
+    path: string
+}
+
+const fileColumns: any[] = ['id', 'name', 'mimetype', 'size', 'path']
+
+
+export async function getAllFile(): Promise<[FileInfo]> {
+    const result = await sql<[FileInfo]>`
+        SELECT ${sql(fileColumns)} FROM file
+    `;
+
+    return result;
+} 
+
+export async function getByID(fileID: string): Promise<[FileInfo]> {
+    const result = await sql<[FileInfo]>`
+        SELECT ${sql(fileColumns)} FROM file
+        WHERE id = '${fileID}'
+    `;
+
+    return result;
+}
+
+export async function save(file: FileInfo) {
+    await sql`
+        INSERT INTO file ${
+            sql(file, fileColumns)
+        }
+    `
+};
--- src/services/sql.ts
+++ src/services/sql.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 const sql = postgres('postgres://username:password@host:port/database', {
     host                 : process.env.DB_HOST || 'localhost',            // Postgres ip address[s] or domain name[s]
-    port                 : process.env.DB_PORT || 5432,          // Postgres server port[s]
+    port                 : 5432,          // Postgres server port[s]
     database             : process.env.DB_NAME || 'simple_file_server_db',            // Name of database to connect to
     username             : process.env.DB_USERNAME || 'simple_file_server_db_user',            // Username of database user
     password             : process.env.DB_PASSWORD || 'simple_file_server_db_user password',            // Password of database user
